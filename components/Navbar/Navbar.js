53142f5a276dbcf5807443b103697f7684216038
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-15 21:53:21 +0900

    feat: added logout functionality to navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import fire from '../../config/fire-conf';
+import { useHistory } from "react-router-dom";
 import Link from '@material-ui/core/Link';
 import styles from './Navbar.module.css';
 import { useRouter } from 'next/router';
 
 const Navbar = ({ loggedIn }) => { 
   const router = useRouter()
+  const history = useHistory();
 
   const goToRoute = (e) => {
     e.preventDefault()
     router.push(e.target.value);
+  }
+  
+  const handleLogout = () => {
+    fire.auth()
+      .signOut()
+      .then(() => {
+        history.push("/");
+      });
   }
 
   return (
@@ -27,15 +36,20 @@ const Navbar = ({ loggedIn }) => {
           </h1>
           <div className={styles.spacer}/>
           <div>
-          {loggedIn
-          ? <h1>logged in</h1>
-          : <h1>logged out</h1>
-        }
-          
+
           <button value="about" className={styles.option} onClick={goToRoute}>About</button>
           <button value="features" color="inherit" className={styles.option} onClick={goToRoute}>Features</button>
+
+          {loggedIn
+          ? <div>
+            <button value="login" color="inherit" className={styles.option}  onClick={handleLogout}>Logout</button>
+            </div>
+          : <div>
           <button value="login" color="inherit" className={styles.option} onClick={goToRoute}>Login</button>
           <button value="register" color="inherit" className={styles.option} onClick={goToRoute}>Sign Up</button>
+          </div>
+        }
+          
           </div>
         </Toolbar>
       </AppBar>
