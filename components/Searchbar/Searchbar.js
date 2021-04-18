c8dc9cc14175b06d6165b86f408a4c85852dca0b
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-18 22:58:06 +0900

    feat: style searchbar

--- components/Searchbar/Searchbar.js
+++ components/Searchbar/Searchbar.js
@@ -1,17 +1,18 @@
 import Search from '@material-ui/icons/Search';
+import styles from "./Searchbar.module.css";
 
 const SearchBar = ({onChange, placeholder}) => {
     return (
-      <div className="Search">
-        <span className="SearchSpan">
-          <Search />
-        </span>
-        <input
-          className="SearchInput"
-          type="text"
-          onChange={onChange}
-          placeholder={placeholder}
-        />
+
+      <div className={styles.wrap}>
+      <div className={styles.search}>
+        <input type="text" className={styles.searchTerm}
+        onChange={onChange}
+        placeholder={placeholder} />
+        <button type="submit" className={styles.searchButton}>
+        <Search />
+        </button>
+      </div>
       </div>
     );
   };
