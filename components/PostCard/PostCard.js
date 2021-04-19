a999e2e428d5b89a2dc39886f3309446b5bf554f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-19 18:37:37 +0900

    feat: change conditional of user post card

--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -4,11 +4,10 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import styles from './PostCard.module.css';
+import fire from '../../config/fire-conf';
 import Avatar from '@material-ui/core/Avatar';
 import React from 'react';
-
 import formatDistance from 'date-fns/formatDistance'
-
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
@@ -21,14 +20,36 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Typography from '@material-ui/core/Typography';
 
 
+const MenuOption = ({isUser}) => {
+  if(isUser){
+    return (
+      <IconButton aria-label="settings">
+                <MoreVertIcon />
+      </IconButton>
+        )
+  }
+  return null;
+}
+
+const JoinButton = ({isUser}) => {
+  if(isUser){
+    return (
+    <Button>JOIN EVENT</Button>
+    )
+  }
+  return null;
+}
+
 
 const PostCard = ({post})  => {
     const router = useRouter()
+    const { currentUser } = fire.auth();
     const [date, setDate] = React.useState(new Date(post.date.seconds));
 
     const goToPost = (postId) => {
         router.push(`/posts/${postId}`);
     };
+
   
     return (
       <Card className={styles.card}>
@@ -37,21 +58,22 @@ const PostCard = ({post})  => {
           <Avatar aria-label="recipe" src={post.photoURL}></Avatar>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          <MenuOption isUser={post.uid == currentUser.uid}></MenuOption>
         }
-        title={post.title}
-        subheader={date.toString()}
+        title={post.name}
+        subheader={formatDistance(new Date(), date)}
       />
 
       <CardContent>
+      <Typography variant="h6" color="textPrimary" component="p">
+          {post.title}
+        </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {post.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <Button>JOIN EVENT</Button>
+        <JoinButton isUser={post.uid != currentUser.uid}></JoinButton>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
