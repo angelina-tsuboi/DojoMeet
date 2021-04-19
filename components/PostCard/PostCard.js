a8a3e07bcf825dcb30dc8356a0517e66d26b7388
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-19 18:19:58 +0900

    feat: restyle post card

--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -3,41 +3,63 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import styles from './PostCard.module.css';
 import Avatar from '@material-ui/core/Avatar';
+import React from 'react';
+
 import formatDistance from 'date-fns/formatDistance'
 
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
 
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Typography from '@material-ui/core/Typography';
 
 
 
 const PostCard = ({post})  => {
     const router = useRouter()
+    const [date, setDate] = React.useState(new Date(post.date.seconds));
 
     const goToPost = (postId) => {
         router.push(`/posts/${postId}`);
     };
   
     return (
-        <Card style={{marginBottom: '1rem'}}>
-        <CardContent onClick={() => goToPost(post.id)}>
-        <Typography variant="h6" component="h5">
-            { formatDistance(new Date(post.time.seconds), new Date()) }
+      <Card className={styles.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" src={post.photoURL}></Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={post.title}
+        subheader={date.toString()}
+      />
+
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.description}
         </Typography>
-        
-          <Typography variant="h5" component="h2">
-            { post.title }
-          </Typography>
-          <Typography color="textSecondary">
-          { post.location }
-          </Typography>
-          <Typography variant="body2" component="p">
-            { post.description }
-          </Typography>
-          <Avatar src={post.photoURL} />
-          <p>{post.name}</p>
-        </CardContent>
-      </Card>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Button>JOIN EVENT</Button>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
     );
   }
 
