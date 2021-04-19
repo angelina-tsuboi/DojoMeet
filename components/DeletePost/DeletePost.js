47fa1cdb52acf2774aec674eccc507c57e208adb
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-19 19:00:22 +0900

    feat: init delete and edit post dialogs

---
+++ components/DeletePost/DeletePost.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import styles from './DeletePost.module.css';
+
+const DeletePost = ({post})  => {
+  
+    return (
+        <div>
+            <h2>Delete Post</h2>
+        </div>
+    );
+  }
+
+  export default DeletePost;
+  
---
+++ components/EditPost/EditPost.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import styles from './EditPost.module.css';
+
+const EditPost = ({post})  => {
+  
+    return (
+        <div>
+            <h2>Edit Post</h2>
+        </div>
+    );
+  }
+
+  export default EditPost;
+  
