d07e33b9fa6a62734956ccc89f2b7928e7b124a4
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-21 23:34:47 +0900

    feat: attempt new profile card

--- pages/players.js
+++ pages/players.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import fire from '../config/fire-conf';
-import {useFirestore} from '../firebase/useFirestore';
+import { useFirestore } from '../firebase/useFirestore';
 import PlayerCard from '../components/PlayerCard/PlayerCard';
 import Fuse from "fuse.js";
 import Grid from '@material-ui/core/Grid';
@@ -19,13 +19,13 @@ const Players = () => {
   });
 
   const onSearchChange = (pattern) => {
-    if(pattern == "") setData(players);
+    if (pattern == "") setData(players);
     const result = fuse.search(pattern);
     const matches = [];
     if (!result.length) {
       setData([]);
     } else {
-      result.forEach(({item}) => {
+      result.forEach(({ item }) => {
         matches.push(item);
       });
       setData(matches);
@@ -51,7 +51,7 @@ const Players = () => {
     })
 
 
-    // calls after every render
+  // calls after every render
   useEffect(() => {
     firestore.getCollection("users", (result) => {
       const userData = result.docs.map(doc => ({
@@ -75,19 +75,35 @@ const Players = () => {
       <Searchbar
         placeholder="Search for players..."
         onChange={(e) => onSearchChange(e.target.value)}
-       />
-      
-  
+      />
+
+
       <Grid container spacing={3} className={styles.playerGrid}>
-      {data.map(player =>
-        <Grid item xs={6} sm={3} key={player.uid}>
-          <PlayerCard player={player} key={player.uid}/>
-        </Grid>
-      )}
-        
+        {data.map(player =>
+          <Grid item xs={6} sm={3} key={player.uid}>
+            <PlayerCard player={player} key={player.uid} />
+          </Grid>
+        )}
+
       </Grid>
 
-        {notification}
+      {notification}
+
+
+      <div class="container">
+        <div className={styles.grid7}>
+          <div className={styles.card}>
+            <img src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7d5363c18112a02ce22d0c46f8570147&auto=format&fit=crop&w=635&q=80%20635w" alt="profile-pic" className={styles.profile} />
+            <h1 className={styles.title}>Bevely Little</h1>
+            <p className={styles.jobTitle}> SENIOR PRODUCT DESIGNER</p>
+            <div className={styles.desc}>
+              <p>Create usable interface and designs @GraphicSpark</p>
+            </div>
+            <button className={styles.btn}> Hire me</button>
+            <hr />
+          </div>
+        </div>
+      </div>
 
     </div>
   )
