6b6666a450aa05f0576cd640f96937caf7f7e141
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-24 23:56:12 +0900

    feat: add content to select belt color

--- pages/register.js
+++ pages/register.js
@@ -100,24 +100,31 @@ const RegisterForm = () => {
 
 const SelectOptions = () => {
   const classes = useStyles();
-  const [belt, setBelt] = useState(10);
+  const [belt, setBelt] = useState("white");
+
+  const handleChange = (event) => {
+    setBelt(event.target.value);
+  };
+
+
   return (
     <div>
 <FormControl variant="outlined" className={classes.formControl}>
-      <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
+      <InputLabel id="demo-simple-select-outlined-label">Belt Color</InputLabel>
       <Select
         labelId="demo-simple-select-outlined-label"
         id="demo-simple-select-outlined"
         value={belt}
         onChange={setBelt}
-        label="Age"
+        label="Select Belt Color..."
       >
-        <MenuItem value="">
-          <em>None</em>
-        </MenuItem>
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        <MenuItem value="white">White</MenuItem>
+        <MenuItem value="orange">Orange</MenuItem>
+        <MenuItem value="blue">Blue</MenuItem>
+        <MenuItem value="green">Green</MenuItem>
+        <MenuItem value="purple">Purple</MenuItem>
+        <MenuItem value="brown">Brown</MenuItem>
+        <MenuItem value="black">Black</MenuItem>
       </Select>
     </FormControl>
 
