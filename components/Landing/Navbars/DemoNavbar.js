8826693dcbe59a4878e5b4677fa59d7166bf140a
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-26 00:38:11 +0900

    feat: attempt adjusting imports

--- components/Landing/Navbars/DemoNavbar.js
+++ components/Landing/Navbars/DemoNavbar.js
@@ -75,7 +75,7 @@ class DemoNavbar extends React.Component {
               <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
                 <img
                   alt="..."
-                  src={require("../../../public/img/brand/argon-react-white.png")}
+                  src="/img/brand/argon-react-white.png"
                 />
               </NavbarBrand>
               <button className="navbar-toggler" id="navbar_global">
@@ -94,7 +94,7 @@ class DemoNavbar extends React.Component {
                       <Link to="/">
                         <img
                           alt="..."
-                          src={require("../../../public/img/brand/argon-react.png")}
+                          src="/img/brand/argon-react.png"
                         />
                       </Link>
                     </Col>
--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ module.exports = {
         },
         use: ['@svgr/webpack'],
       });
+
+      config.module.rules.push(
+        {
+          test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+          loader: 'url-loader?limit=100000' 
+        }
+      );
   
       return config;
     },
