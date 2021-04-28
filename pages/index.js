d8f5a7bf7fd940fdaf2d11517eb7ded67af05496
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-28 23:27:25 +0900

    feat: added profile code to home

--- pages/index.js
+++ pages/index.js
@@ -152,81 +152,209 @@ const Home = () => {
         ?
         <HomePage />
         :
-        <div>
-          <Grid container spacing={3}>
-            {currentUser && 
-            <Grid item xs={6}>
-            <Card className={styles.profileCard} style={{ width: '70%', margin: 'auto', marginTop: '1rem' }}>
-              <Avatar aria-label="recipe" src={currentUser.photoURL} style={{ height: '100px', width: '100px' }} className={styles.profileImage}></Avatar>
-              <CardContent>
-                <Typography variant="h4" color="textPrimary" component="p">
-                  {name}
-                </Typography>
-                <Typography variant="h6" color="textSecondary" component="p">
-                  {location}
-                </Typography>
-                <Typography variant="body1" color="textSecondary" component="p">
-                  {description}
-                </Typography>
-                <Button onClick={handleClickOpen}>
-                  Edit Profile
-            </Button>
+    //     <div>
+    //       <Grid container spacing={3}>
+    //         {currentUser && 
+    //         <Grid item xs={6}>
+    //         <Card className={styles.profileCard} style={{ width: '70%', margin: 'auto', marginTop: '1rem' }}>
+    //           <Avatar aria-label="recipe" src={currentUser.photoURL} style={{ height: '100px', width: '100px' }} className={styles.profileImage}></Avatar>
+    //           <CardContent>
+    //             <Typography variant="h4" color="textPrimary" component="p">
+    //               {name}
+    //             </Typography>
+    //             <Typography variant="h6" color="textSecondary" component="p">
+    //               {location}
+    //             </Typography>
+    //             <Typography variant="body1" color="textSecondary" component="p">
+    //               {description}
+    //             </Typography>
+    //             <Button onClick={handleClickOpen}>
+    //               Edit Profile
+    //         </Button>
 
-                <Button onClick={handleLogout}>
-                  Logout
-            </Button>
-              </CardContent>
-            </Card>
-          </Grid>
-          }
+    //             <Button onClick={handleLogout}>
+    //               Logout
+    //         </Button>
+    //           </CardContent>
+    //         </Card>
+    //       </Grid>
+    //       }
             
-            <Grid item xs={6}>
+    //         <Grid item xs={6}>
 
-              <h3>Your Upcoming Events</h3>
-              <List>
-                {posts.map(post =>
-                  <div key={post.id}>
-                    <ListItem key={post.id}>
-                      <ListItemAvatar>
-                        <Avatar>
-                          <ImageIcon />
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText primary={post.title} secondary="Jan 9, 2014" />
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
+    //           <h3>Your Upcoming Events</h3>
+    //           <List>
+    //             {posts.map(post =>
+    //               <div key={post.id}>
+    //                 <ListItem key={post.id}>
+    //                   <ListItemAvatar>
+    //                     <Avatar>
+    //                       <ImageIcon />
+    //                     </Avatar>
+    //                   </ListItemAvatar>
+    //                   <ListItemText primary={post.title} secondary="Jan 9, 2014" />
+    //                 </ListItem>
+    //                 <Divider variant="inset" component="li" />
+    //               </div>
+
+    //             )}
+    //           </List>
+
+    //           <h3>Your Events</h3>
+    //           {userPosts.map(post =>
+    //             <div key={post.id}>
+    //               <ListItem key={post.id}>
+    //                 <ListItemAvatar>
+    //                   <Avatar>
+    //                     <ImageIcon />
+    //                   </Avatar>
+    //                 </ListItemAvatar>
+    //                 <ListItemText primary={post.title} secondary="Jan 9, 2014" />
+    //               </ListItem>
+    //               {/* <Divider variant="inset" component="li" /> */}
+    //             </div>
+    //           )}
+
+
+    //         </Grid>
+    //       </Grid>
+
+    //     </div>
+
+    //   }
+    //   {(currentUser && userData) && 
+    //     <EditProfile open={open} onClose={handleClose} profile={userData}/>
+    //   }
+
+    <main className="profile-page" ref="main">
+          <section className="section-profile-cover section-shaped my-0">
+            {/* Circles background */}
+            <div className="shape shape-style-1 shape-default alpha-4">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          <section className="section">
+            <Container>
+              <Card className="card-profile shadow mt--300">
+                <div className="px-4">
+                  <Row className="justify-content-center">
+                    <Col className="order-lg-2" lg="3">
+                      <div className="card-profile-image">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                          <img
+                            alt="..."
+                            className="rounded-circle"
+                            src={require("assets/img/theme/team-4-800x800.jpg")}
+                          />
+                        </a>
+                      </div>
+                    </Col>
+                    <Col
+                      className="order-lg-3 text-lg-right align-self-lg-center"
+                      lg="4"
+                    >
+                      <div className="card-profile-actions py-4 mt-lg-0">
+                        <Button
+                          className="mr-4"
+                          color="info"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                          size="sm"
+                        >
+                          Connect
+                        </Button>
+                        <Button
+                          className="float-right"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                          size="sm"
+                        >
+                          Message
+                        </Button>
+                      </div>
+                    </Col>
+                    <Col className="order-lg-1" lg="4">
+                      <div className="card-profile-stats d-flex justify-content-center">
+                        <div>
+                          <span className="heading">22</span>
+                          <span className="description">Friends</span>
+                        </div>
+                        <div>
+                          <span className="heading">10</span>
+                          <span className="description">Photos</span>
+                        </div>
+                        <div>
+                          <span className="heading">89</span>
+                          <span className="description">Comments</span>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                  <div className="text-center mt-5">
+                    <h3>
+                      Jessica Jones{" "}
+                      <span className="font-weight-light">, 27</span>
+                    </h3>
+                    <div className="h6 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Bucharest, Romania
+                    </div>
+                    <div className="h6 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      Solution Manager - Creative Tim Officer
+                    </div>
+                    <div>
+                      <i className="ni education_hat mr-2" />
+                      University of Computer Science
+                    </div>
                   </div>
-
-                )}
-              </List>
-
-              <h3>Your Events</h3>
-              {userPosts.map(post =>
-                <div key={post.id}>
-                  <ListItem key={post.id}>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <ImageIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={post.title} secondary="Jan 9, 2014" />
-                  </ListItem>
-                  {/* <Divider variant="inset" component="li" /> */}
+                  <div className="mt-5 py-5 border-top text-center">
+                    <Row className="justify-content-center">
+                      <Col lg="9">
+                        <p>
+                          An artist of considerable range, Ryan — the name taken
+                          by Melbourne-raised, Brooklyn-based Nick Murphy —
+                          writes, performs and records all of his own music,
+                          giving it a warm, intimate feel with a solid groove
+                          structure. An artist of considerable range.
+                        </p>
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                          Show more
+                        </a>
+                      </Col>
+                    </Row>
+                  </div>
                 </div>
-              )}
-
-
-            </Grid>
-          </Grid>
-
-        </div>
-
-      }
-      {(currentUser && userData) && 
-        <EditProfile open={open} onClose={handleClose} profile={userData}/>
-      }
-
+              </Card>
+            </Container>
+          </section>
+        </main>
+}
     </div>
-  )
+  
+)
 }
 export default Home;
