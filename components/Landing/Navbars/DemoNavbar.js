752913d09171e896fb6f778ae5d941cdcb99309f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-28 23:57:30 +0900

    fix: bugs with auth navbar code

--- components/Landing/Navbars/DemoNavbar.js
+++ components/Landing/Navbars/DemoNavbar.js
@@ -121,73 +121,71 @@ class DemoNavbar extends React.Component {
               </Navbar>
             </header>
           );
-        } else {
-          return (
-            <header className="header-global">
-              <Navbar className="navbar-main navbar-transparent navbar-light headroom" expand="lg" id="navbar-main">
-                <Container>
-                  <NavbarBrand href="#pablo" onClick={e => e.preventDefault()}>
-                    Primary Color
-            </NavbarBrand>
-                  <button className="navbar-toggler" id="navbar-primary">
-                    <span className="navbar-toggler-icon" />
-                  </button>
-                  <UncontrolledCollapse navbar toggler="#navbar-primary">
-                    <div className="navbar-collapse-header">
-                      <Row>
-                        <Col className="collapse-brand" xs="6">
-                        </Col>
-                        <Col className="collapse-close" xs="6">
-                          <button className="navbar-toggler" id="navbar-primary">
-                            <span />
-                            <span />
-                          </button>
-                        </Col>
-                      </Row>
-                    </div>
-                    <Nav className="ml-lg-auto" navbar>
-                      <NavItem>
-                        <NavLink href="about" onClick={e => goToRoute(e)} >
-                          About <span className="sr-only">(current)</span>
-                        </NavLink>
-                      </NavItem>
-                      <NavItem>
-                        <NavLink href="features" onClick={e => goToRoute(e)} >
-                          Features <span className="sr-only">(current)</span>
-                        </NavLink>
-                      </NavItem>
-                      <NavItem>
-                        <NavLink href="login" onClick={e => goToRoute(e)}>
-                          Login
-                  </NavLink>
-                      </NavItem>
-
-                    </Nav>
-                  </UncontrolledCollapse>
-
-                  <NavItem className="d-none d-lg-block ml-lg-4">
-                    <Button
-                      className="btn-neutral btn-icon"
-                      color="default"
-                      href="signup"
-                      onClick={e => goToRoute(e)}
-                    >
-                      <span className="btn-inner--icon">
-                        <i className="fa fa-cloud-download mr-2" />
-                      </span>
-                      <span className="nav-link-inner--text ml-1">
-                        Get Started
-                    </span>
-                    </Button>
-                  </NavItem>
-                </Container>
-              </Navbar>
-            </header>
-          );
-        }
+        } 
       })
 
-      return (<h1>Loading...</h1>)
+      return (
+        <header className="header-global">
+          <Navbar className="navbar-main navbar-transparent navbar-light headroom" expand="lg" id="navbar-main">
+            <Container>
+              <NavbarBrand href="#pablo" onClick={e => e.preventDefault()}>
+                Primary Color
+        </NavbarBrand>
+              <button className="navbar-toggler" id="navbar-primary">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse navbar toggler="#navbar-primary">
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar-primary">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+                <Nav className="ml-lg-auto" navbar>
+                  <NavItem>
+                    <NavLink href="about" onClick={e => goToRoute(e)} >
+                      About <span className="sr-only">(current)</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="features" onClick={e => goToRoute(e)} >
+                      Features <span className="sr-only">(current)</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="login" onClick={e => goToRoute(e)}>
+                      Login
+              </NavLink>
+                  </NavItem>
+
+                </Nav>
+              </UncontrolledCollapse>
+
+              <NavItem className="d-none d-lg-block ml-lg-4">
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="signup"
+                  onClick={e => goToRoute(e)}
+                >
+                  <span className="btn-inner--icon">
+                    <i className="fa fa-cloud-download mr-2" />
+                  </span>
+                  <span className="nav-link-inner--text ml-1">
+                    Get Started
+                </span>
+                </Button>
+              </NavItem>
+            </Container>
+          </Navbar>
+        </header>
+      )
   }
 }
 
