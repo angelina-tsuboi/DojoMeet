d57cb929cedd1cf3b6aff57fa5eba86b50e367b8
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-29 16:10:11 +0900

    feat: added new login template

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import fire from '../config/fire-conf';
 import { useRouter } from 'next/router';
 import Link from '@material-ui/core/Link';
 import styles from '../styles/Login.module.css';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -14,7 +14,23 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+// import Container from '@material-ui/core/Container';
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 
 function Copyright() {
   return (
@@ -106,79 +122,220 @@ const Login = () => {
   }
   
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
+    // <Container component="main" maxWidth="xs">
+    //   <CssBaseline />
+    //   <div className={classes.paper}>
+    //     <Avatar className={classes.avatar}>
+    //       <LockOutlinedIcon />
+    //     </Avatar>
+    //     <Typography component="h1" variant="h5">
+    //       Sign in
+    //     </Typography>
+    //     <form className={classes.form} noValidate>
+    //       <TextField
+    //         variant="outlined"
+    //         margin="normal"
+    //         required
+    //         fullWidth
+    //         id="email"
+    //         label="Email Address"
+    //         name="email"
+    //         autoComplete="email"
+    //         autoFocus
+    //       />
+    //       <TextField
+    //         variant="outlined"
+    //         margin="normal"
+    //         required
+    //         fullWidth
+    //         name="password"
+    //         label="Password"
+    //         type="password"
+    //         id="password"
+    //         autoComplete="current-password"
+    //       />
         
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
+    //       <Button
+    //         type="submit"
+    //         fullWidth
+    //         variant="contained"
+    //         color="primary"
+    //         className={classes.submit}
+    //       >
+    //         Sign In
+    //       </Button>
 
-          <Button
-            type="button"
-            onClick={handleGoogleLogin}
-            fullWidth
-            variant="contained"
-            className={classes.submit}
-          >
-           Google Login
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+    //       <Button
+    //         type="button"
+    //         onClick={handleGoogleLogin}
+    //         fullWidth
+    //         variant="contained"
+    //         className={classes.submit}
+    //       >
+    //        Google Login
+    //       </Button>
+    //       <Grid container>
+    //         <Grid item xs>
+    //           <Link href="#" variant="body2">
+    //             Forgot password?
+    //           </Link>
+    //         </Grid>
+    //         <Grid item>
+    //           <Link href="#" variant="body2">
+    //             {"Don't have an account? Sign Up"}
+    //           </Link>
+    //         </Grid>
+    //       </Grid>
+    //     </form>
 
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+    //   </div>
+    //   <Box mt={8}>
+    //     <Copyright />
+    //   </Box>
 
      
-    </Container>
+    // </Container>
+
+    <main>
+          <section className="section section-shaped section-lg">
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="pt-lg-7">
+              <Row className="justify-content-center">
+                <Col lg="5">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardHeader className="bg-white pb-5">
+                      <div className="text-muted text-center mb-3">
+                        <small>Sign up with</small>
+                      </div>
+                      <div className="text-center">
+                        <Button
+                          className="btn-neutral btn-icon mr-4"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <span className="btn-inner--icon mr-1">
+                            <img
+                              alt="..."
+                              src="/img/icons/common/github.svg"
+                            />
+                          </span>
+                          <span className="btn-inner--text">Github</span>
+                        </Button>
+                        <Button
+                          className="btn-neutral btn-icon ml-1"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <span className="btn-inner--icon mr-1">
+                            <img
+                              alt="..."
+                              src="/img/icons/common/google.svg"
+                            />
+                          </span>
+                          <span className="btn-inner--text">Google</span>
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <CardBody className="px-lg-5 py-lg-5">
+                      <div className="text-center text-muted mb-4">
+                        <small>Or sign up with credentials</small>
+                      </div>
+                      <Form role="form">
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-hat-3" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Name" type="text" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-email-83" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Email" type="email" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              placeholder="Password"
+                              type="password"
+                              autoComplete="off"
+                            />
+                          </InputGroup>
+                        </FormGroup>
+                        <div className="text-muted font-italic">
+                          <small>
+                            password strength:{" "}
+                            <span className="text-success font-weight-700">
+                              strong
+                            </span>
+                          </small>
+                        </div>
+                        <Row className="my-4">
+                          <Col xs="12">
+                            <div className="custom-control custom-control-alternative custom-checkbox">
+                              <input
+                                className="custom-control-input"
+                                id="customCheckRegister"
+                                type="checkbox"
+                              />
+                              <label
+                                className="custom-control-label"
+                                htmlFor="customCheckRegister"
+                              >
+                                <span>
+                                  I agree with the{" "}
+                                  <a
+                                    href="#pablo"
+                                    onClick={e => e.preventDefault()}
+                                  >
+                                    Privacy Policy
+                                  </a>
+                                </span>
+                              </label>
+                            </div>
+                          </Col>
+                        </Row>
+                        <div className="text-center">
+                          <Button
+                            className="mt-4"
+                            color="primary"
+                            type="button"
+                          >
+                            Create account
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </main>
   )
 }
 export default Login;
