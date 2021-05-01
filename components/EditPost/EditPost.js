76bb9caf38afe7e783b54d9547cae1a5aa25be6d
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-01 13:59:58 -0700

    feat: convert EditPost into class based component

--- components/EditPost/EditPost.js
+++ components/EditPost/EditPost.js
@@ -1,17 +1,12 @@
 
 import React from 'react';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import DateFnsUtils from '@date-io/date-fns';
 import fire from '../../config/fire-conf';
 import { useFirestore } from '../../firebase/useFirestore';
 import RoomIcon from '@material-ui/icons/Room';
 import SubjectIcon from '@material-ui/icons/Subject';
 import styles from './EditPost.module.css';
-const firestore = useFirestore(); 
+const firestore = useFirestore();
 
 import {
   Button,
@@ -32,194 +27,204 @@ import {
 } from '@material-ui/pickers';
 import 'date-fns';
 
-const EditPost = ({post, open, onClose})  => {
-    const { currentUser } = fire.auth();
-    const [selectedDate, setSelectedDate] = React.useState(post.date.toDate());
-    const [selectedTime, setSelectedTime] = React.useState(post.time.toDate());
+class EditPost extends React.Component {
+  state = {
+    title: this.props.post.title,
+    description: this.props.post.description,
+    location: this.props.post.location,
+    selectedDate: this.props.post.selectedDate,
+    selectedTime: this.props.post.selectedTime,
+    userData: {
+      uid: this.props.uid,
+      email: this.props.email,
+      name: this.props.name,
+      photoURL: this.props.photoURL
+    },
+    onClose: this.props.onClose,
+    selectedValue: this.props.selectedValue,
+    open: this.props.open
+  };
 
-    const [title, setTitle] = React.useState(post.title);
-    const [description, setDescription] = React.useState(post.description);
-    const [location, setLocation] = React.useState(post.location);
-    const handleClose = () => {
-        onClose();
-    };
+  handleClose = () => {
+    onClose();
+  };
 
-    const updatePost = () => {
-      let postData = {description: description, location: location, date: selectedDate, time: selectedTime, title: title};
-      firestore.updateDocument(`posts/${post.id}`, postData, (result) => {
-        onClose();
-      })
-    }
+  updatePost = () => {
+    let postData = { description: description, location: location, date: selectedDate, time: selectedTime, title: title };
+    firestore.updateDocument(`posts/${post.id}`, postData, (result) => {
+      onClose();
+    })
+  }
 
-    const handleDateChange = (date) => {
-      setSelectedDate(date);
-    };
-  
-    const handleTimeChange = (time) => {
-      setSelectedTime(time);
-    };
-    
-    
-      return (
-    //     <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-    //       <div className={styles.Dialog}>
-    //     <DialogTitle id="simple-dialog-title">Update Event</DialogTitle>
-    //     <TextField id="standard-basic" label="Event Title" value={title} onChange= {({target}) => setTitle(target.value)}  />
-    //     <MuiPickersUtilsProvider utils={DateFnsUtils}>
-    //   <Grid container justify="space-between">
-    //     <KeyboardDatePicker
-    //       disableToolbar
-    //       variant="inline"
-    //       format="MM/dd/yyyy"
-    //       margin="normal"
-    //       id="date-picker-inline"
-    //       label="Select Date"
-    //       value={selectedDate}
-    //       onChange={handleDateChange}
-    //       KeyboardButtonProps={{
-    //         'aria-label': 'change date',
-    //       }}
-    //     />
-    //     <KeyboardTimePicker
-    //       margin="normal"
-    //       id="time-picker"
-    //       label="Select Time"
-    //       value={selectedTime}
-    //       onChange={handleTimeChange}
-    //       KeyboardButtonProps={{
-    //         'aria-label': 'change time',
-    //       }}
-    //     />
-    //   </Grid>
-    // </MuiPickersUtilsProvider>
-    // <Grid container spacing={1} alignItems="flex-end">
-    //       <Grid item>
-    //         <RoomIcon />
-    //       </Grid>
-    //       <Grid item>
-    //         <TextField id="input-with-icon-grid" label="Enter location" value={location} onChange= {({target}) => setLocation(target.value)} />
-    //       </Grid>
-    //     </Grid>
-    //     <Grid container spacing={1} alignItems="flex-end">
-    //       <Grid item>
-    //         <SubjectIcon />
-    //       </Grid>
-    //       <Grid item>
-    //         <TextField id="input-with-icon-grid" label="Enter description..." value={description} onChange= {({target}) => setDescription(target.value)} />
-    //       </Grid>
-    //     </Grid>
+  handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-    //     <div className={styles.bottomButtons}>
-    //         <Button onClick={() => handleClose()}>Cancel</Button>
-    //         <Button variant="contained" className={styles.createEvent} onClick={() => { updatePost() }} >
-    //         Update Event
-    //         </Button>
-    //     </div>
-    //     </div>
-    //   </Dialog>
-    <Modal
-    className="modal-dialog-centered"
-    isOpen={this.props.open}
-    toggle={() => this.handleClose()}
-  >
-    <div className="modal-header">
-      <h6 className="modal-title" id="modal-title-default">
-        Create Event
-  </h6>
-      <button
-        aria-label="Close"
-        className="close"
-        data-dismiss="modal"
-        type="button"
-        onClick={() => this.handleClose()}
+  handleTimeChange = (time) => {
+    setSelectedTime(time);
+  };
+
+  render() {
+    return (
+      //     <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      //       <div className={styles.Dialog}>
+      //     <DialogTitle id="simple-dialog-title">Update Event</DialogTitle>
+      //     <TextField id="standard-basic" label="Event Title" value={title} onChange= {({target}) => setTitle(target.value)}  />
+      //     <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      //   <Grid container justify="space-between">
+      //     <KeyboardDatePicker
+      //       disableToolbar
+      //       variant="inline"
+      //       format="MM/dd/yyyy"
+      //       margin="normal"
+      //       id="date-picker-inline"
+      //       label="Select Date"
+      //       value={selectedDate}
+      //       onChange={handleDateChange}
+      //       KeyboardButtonProps={{
+      //         'aria-label': 'change date',
+      //       }}
+      //     />
+      //     <KeyboardTimePicker
+      //       margin="normal"
+      //       id="time-picker"
+      //       label="Select Time"
+      //       value={selectedTime}
+      //       onChange={handleTimeChange}
+      //       KeyboardButtonProps={{
+      //         'aria-label': 'change time',
+      //       }}
+      //     />
+      //   </Grid>
+      // </MuiPickersUtilsProvider>
+      // <Grid container spacing={1} alignItems="flex-end">
+      //       <Grid item>
+      //         <RoomIcon />
+      //       </Grid>
+      //       <Grid item>
+      //         <TextField id="input-with-icon-grid" label="Enter location" value={location} onChange= {({target}) => setLocation(target.value)} />
+      //       </Grid>
+      //     </Grid>
+      //     <Grid container spacing={1} alignItems="flex-end">
+      //       <Grid item>
+      //         <SubjectIcon />
+      //       </Grid>
+      //       <Grid item>
+      //         <TextField id="input-with-icon-grid" label="Enter description..." value={description} onChange= {({target}) => setDescription(target.value)} />
+      //       </Grid>
+      //     </Grid>
+
+      //     <div className={styles.bottomButtons}>
+      //         <Button onClick={() => handleClose()}>Cancel</Button>
+      //         <Button variant="contained" className={styles.createEvent} onClick={() => { updatePost() }} >
+      //         Update Event
+      //         </Button>
+      //     </div>
+      //     </div>
+      //   </Dialog>
+      <Modal
+        className="modal-dialog-centered"
+        isOpen={this.props.open}
+        toggle={() => this.handleClose()}
       >
-        <span aria-hidden={true}>×</span>
-      </button>
-    </div>
-    <div className="modal-body">
-    <Label for="exampleText">Event Title</Label>
-      <Input placeholder="Event Title..." type="text" onChange= {({target}) => this.setState({title: target.value})}/>
+        <div className="modal-header">
+          <h6 className="modal-title" id="modal-title-default">
+            Create Event
+      </h6>
+          <button
+            aria-label="Close"
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => this.handleClose()}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <Label for="exampleText">Event Title</Label>
+          <Input placeholder="Event Title..." type="text" onChange={({ target }) => this.setState({ title: target.value })} />
 
-      <Label for="exampleText">Event Location</Label>
-      <InputGroup className="mb-4">
-        <InputGroupAddon addonType="prepend">
-          <InputGroupText>
-            <i className="ni ni-zoom-split-in" />
-          </InputGroupText>
-        </InputGroupAddon>
-        <Input
-          placeholder="Event Location..."
-          type="text"
-          onChange= {({target}) => this.setState({location: target.value})}
-        />
-      </InputGroup>
-
-      <Label for="exampleText">Event Time</Label>
-      <Row>
-        <Col md="6">
-          <InputGroup>
+          <Label for="exampleText">Event Location</Label>
+          <InputGroup className="mb-4">
             <InputGroupAddon addonType="prepend">
               <InputGroupText>
-                <i className="ni ni-calendar-grid-58" />
+                <i className="ni ni-zoom-split-in" />
               </InputGroupText>
             </InputGroupAddon>
-            <ReactDatetime
-              inputProps={{
-                placeholder: "Date Picker Here"
-              }}
-              timeFormat={false}
-              onChange={e =>
-                this.setState({selectedDate: e.toDate()})
-              }
+            <Input
+              placeholder="Event Location..."
+              type="text"
+              onChange={({ target }) => this.setState({ location: target.value })}
             />
           </InputGroup>
-        </Col>
 
-        <Col md="6">
+          <Label for="exampleText">Event Time</Label>
+          <Row>
+            <Col md="6">
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-calendar-grid-58" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <ReactDatetime
+                  inputProps={{
+                    placeholder: "Date Picker Here"
+                  }}
+                  timeFormat={false}
+                  onChange={e =>
+                    this.setState({ selectedDate: e.toDate() })
+                  }
+                />
+              </InputGroup>
+            </Col>
+
+            <Col md="6">
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-calendar-grid-58" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <ReactDatetime
+                  inputProps={{
+                    placeholder: "Time Picker Here"
+                  }}
+                  dateFormat={false}
+
+                  onChange={e =>
+                    this.setState({ selectedTime: e.toDate() })
+                  }
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+
+          <Label for="exampleText">Event Description</Label>
           <InputGroup>
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>
-                <i className="ni ni-calendar-grid-58" />
-              </InputGroupText>
-            </InputGroupAddon>
-            <ReactDatetime
-              inputProps={{
-                placeholder: "Time Picker Here"
-              }}
-              dateFormat={false}
-
-              onChange={e =>
-                this.setState({selectedTime: e.toDate()})
-              }
-            />
+            <Input type="textarea" name="text" id="exampleText" onChange={({ target }) => this.setState({ description: target.value })} />
           </InputGroup>
-        </Col>
-      </Row>
 
-      <Label for="exampleText">Event Description</Label>
-      <InputGroup>
-        <Input type="textarea" name="text" id="exampleText" onChange= {({target}) => this.setState({description: target.value})}/>
-      </InputGroup>
-
-    </div>
-    <div className="modal-footer">
-      <Button color="primary" type="button" onClick={() => this.handleCreatePost()} disabled={this.formDisabled()}>
-        Create Event
+        </div>
+        <div className="modal-footer">
+          <Button color="primary" type="button" onClick={() => this.handleCreatePost()} disabled={this.formDisabled()}>
+            Create Event
+          </Button>
+          <Button
+            className="ml-auto"
+            color="link"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => this.handleClose()}
+          >
+            Close
       </Button>
-      <Button
-        className="ml-auto"
-        color="link"
-        data-dismiss="modal"
-        type="button"
-        onClick={() => this.handleClose()}
-      >
-        Close
-  </Button>
-    </div>
-  </Modal>
+        </div>
+      </Modal>
 
-      );
+    );
+  }
 }
 
-  export default EditPost;
-  
+export default EditPost;
