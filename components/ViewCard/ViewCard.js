72a1e8d903f49dcda89e2648956f4c2b42e21c25
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-01 13:49:42 -0700

    feat: added openpost func to View Card

--- components/ViewCard/ViewCard.js
+++ components/ViewCard/ViewCard.js
@@ -6,52 +6,60 @@ import Avatar from '@material-ui/core/Avatar';
 import { useEffect, useState, Fragment, useContext } from 'react';
 import formatDistance from 'date-fns/formatDistance';
 import CardHeader from '@material-ui/core/CardHeader';
-import {useFirestore} from '../../firebase/useFirestore';
-import {format} from 'date-fns';
+import { useFirestore } from '../../firebase/useFirestore';
+import { format } from 'date-fns';
 import Typography from '@material-ui/core/Typography';
-import {UserDataContext} from '../../providers/userdataprovider';
+import { UserDataContext } from '../../providers/userdataprovider';
 const firestore = useFirestore();
 
 
-const ViewCard = ({ post }) => {  
+const ViewCard = ({ post }) => {
   const router = useRouter()
   const userData = useContext(UserDataContext);
   const [date, setDate] = useState(post.date.toDate());
 
+  const openPost = (id) => {
+    console.log("opening", id)
+    router.push(`/posts/${id}`);
+  }
+
   //TODO: shorten title + description if needed
 
-  if(userData){
+  if (userData) {
     return (
       <div>
-  <Card className={styles.card}> 
-  <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" src={post.photoURL} className={styles.avatar}></Avatar>
-          }
-          title={post.name}
-          subheader={format(post.date.toDate(), 'MM/dd/yyyy') + " at " + format(post.time.toDate(), "HH:mm aaaaa'm'")}
-          className={styles.cardHeader}
-        /> 
-        <CardContent className={styles.cardBody}>
-          <Typography variant="h6" color="textPrimary" component="p">
-            {post.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {post.description}
-          </Typography>
-          {post.joining &&  <Typography variant="body2" color="textSecondary" component="p">
-            {post.joining.length} joining
+        <Card className={styles.card}>
+          <div className={styles.cardHover} onClick={() => { openPost(post.id) }}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" src={post.photoURL} className={styles.avatar}></Avatar>
+              }
+              title={post.name}
+              subheader={format(post.date.toDate(), 'MM/dd/yyyy') + " at " + format(post.time.toDate(), "HH:mm aaaaa'm'")}
+              className={styles.cardHeader}
+            />
+            <CardContent className={styles.cardBody}>
+              <Typography variant="h6" color="textPrimary" component="p">
+                {post.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {post.description}
+              </Typography>
+              {post.joining && <Typography variant="body2" color="textSecondary" component="p">
+                {post.joining.length} joining
           </Typography>}
-         
-        </CardContent>
-      </Card>
-    
+
+            </CardContent>
+          </div>
+
+        </Card>
+
       </div>
     );
-  }else{
+  } else {
     return (<h1>Loading...</h1>)
   }
-  
+
 }
 
 export default ViewCard;
